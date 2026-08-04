7ef2b29065c139ab0c3eb2af2d7706f125cf1537
Joel-Wee <joelwnambiar@gmail.com>
2026-08-04 17:05:06 +0800

    Completed Day 1 Exercises

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -23,3 +23,49 @@ print(type('Asabeneh'))          # String
 print(type([1, 2, 3]))           # List
 print(type({'name': 'Asabeneh'}))  # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Tuple
+
+#Exercise Level 1
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#3
+print("Joel")
+print("Nambiar")
+print("Malaysia")
+print("I am enjoying 30 days of python")
+
+#4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Joel"))
+print(type("Nambiar"))
+print(type("Malaysia"))
+
+#Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary
+print(type(1))
+print(type(3.14))
+print(type(4j))
+print(type("joel"))
+print(type(True))
+print(type([1,2,3,4,5]))
+print(type(("joel", "one", "two")))
+print(type({1,2,3,4}))
+print(type({
+    "year": "2016",
+    "month": "Jan",
+}))
+
+#Find an Euclidean distance between (2, 3) and (10, 8)
+import math
+
+distance = math.sqrt((10-2)**2 + (8-3)**2)
+print(distance)
