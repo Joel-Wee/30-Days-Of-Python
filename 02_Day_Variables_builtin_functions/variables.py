856013c54d923297da0ebaa1e935e1d345278857
Joel-Wee <joelwnambiar@gmail.com>
2026-08-05 16:13:56 +0800

    Completed day 2 exercises

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,40 +1,46 @@
+# Day 2: 30 Days of python programming
+first_name = "Joel"
+last_name = "Nambiar"
+full_name = "Joel Wee Nambair"
+country = "Malaysia"
+city = "KL"
+age = 19
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+family, device, pet = 5, "PC", "Fish"
 
-# Variables in Python
+#Exercise 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(family), type(device), type(pet))
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
-person_info = {
-    'firstname': 'Asabeneh',
-    'lastname': 'Yetayeh',
-    'country': 'Finland',
-    'city': 'Helsinki'
-}
+print(len(first_name))
+print(len(first_name) - len(last_name))
 
-# Printing the values stored in the variables
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
 
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
+radius = 30
+area_of_circle = 3.14*(radius)**2
+circum_of_circle = 2*(3.14)*(radius)
+user_radius = float(input("Enter your radius >> "))
+user_area = 3.14*(user_radius)**2
 
-# Declaring multiple variables in one line
-
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
-
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+help("keywords")
